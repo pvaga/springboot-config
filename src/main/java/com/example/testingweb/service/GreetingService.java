7f6eaa1849918d544196689cd2f8cf0521a3f9e2
pvaga <prashanth.vaga@mass.gov>
2022-01-16 12:21:40 -0500

    Added Logging config.

--- src/main/java/com/example/testingweb/service/GreetingService.java
+++ src/main/java/com/example/testingweb/service/GreetingService.java
@@ -1,10 +1,16 @@
 package com.example.testingweb.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GreetingService {
+	
+	Logger logger = LoggerFactory.getLogger(GreetingService.class);
+	
 	public String greet() {
+		logger.trace("Called Logging Service");
 		return "Hello, World from Service";
 	}
 }
